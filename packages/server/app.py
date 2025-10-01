490e7cbd610690f1bf2752c31a04ced5b0a64c21
Ivan Milos <ivanmilos24@gmail.com>
2025-10-01 17:43:05 +0200

    create the backend

--- packages/server/app.py
+++ packages/server/app.py
@@ -9,7 +9,7 @@ port = int(os.getenv("PORT"))
 
 @app.route("/")
 def home():
-    return os.getenv("OPENAI_API_KEY", "No API key found")
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=True)
