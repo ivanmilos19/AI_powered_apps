5eca1c82caf1f96fd1566bd215fd304857c3fc36
Ivan Milos <ivanmilos24@gmail.com>
2025-10-02 12:11:32 +0200

    Connect the frontend and the backend

--- packages/server/app.py
+++ packages/server/app.py
@@ -1,15 +1,18 @@
-from flask import Flask
 from dotenv import load_dotenv
+from flask import Flask, jsonify
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
-port = int(os.getenv("PORT"))
 
 @app.route("/")
 def home():
     return "Hello World"
 
+@app.route("/api/hello")
+def hello():
+    return jsonify({"message": "Hello World!"})
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
