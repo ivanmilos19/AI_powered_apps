0d2070e3b92d621ba0edb05205c400c3deab502f
Ivan Milos <ivanmilos24@gmail.com>
2025-10-07 16:52:08 +0200

    Build the frontend for the reviews

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,11 @@
 import ChatBot from './components/chat/ChatBot';
+import ReviewList from './components/reviews/ReviewList';
 
 function App() {
+   const mode = import.meta.env.VITE_APP_MODE;
    return (
       <div className="p-4 h-screen">
-         <ChatBot></ChatBot>
+         {mode === 'chatbot' ? <ChatBot /> : <ReviewList productId={2} />}
       </div>
    );
 }
